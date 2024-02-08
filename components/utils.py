48e61ee091a13d72e53db6d7cd0c782393fe29ca
Eleftherios Fanioudakis <eleftherios.fanioudakis@gmail.com>
2024-02-08 14:39:42 +0000

    add text utils

---
+++ components/utils.py
@@ -0,0 +1,19 @@
+import re
+import emoji
+
+
+def remove_emojis(text):
+    allchars = [str for str in text]
+    emoji_list = [c for c in allchars if c in emoji.EMOJI_DATA]
+    clean_text = ' '.join([str for str in text.split() if not any(i in str for i in emoji_list)])
+    return clean_text
+
+
+def remove_multiple_dots(text):
+    result_str = re.sub(r'\.{2,}', '.', text)
+    return result_str
+
+
+def remove_inside_backticks(text):
+    pattern = r'```.*?```'
+    return re.sub(pattern, '', text, flags=re.DOTALL)
