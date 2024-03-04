c508f824ba971cf931e645ca19bcd90624b48339
Eleftherios Fanioudakis <eleftherios.fanioudakis@gmail.com>
2024-03-04 23:23:20 +0000

    fix emoji removal

--- components/utils.py
+++ components/utils.py
@@ -1,10 +1,21 @@
 import re
-import emoji
 
 
 def remove_emojis(text):
-    clean_text = ''.join([c for c in text if c not in emoji.EMOJI_DATA])
-    return clean_text
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F700-\U0001F77F"  # alchemical symbols
+        u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+        u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+        u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+        u"\U0001FA00-\U0001FA6F"  # Chess Symbols
+        u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+        u"\U00002702-\U000027B0"  # Dingbats
+        u"\U000024C2-\U0001F251"  # Miscellaneous Symbols and Pictographs
+        "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
 
 
 def remove_multiple_dots(text):
