4621395334d5104d3ba0b1f8ef6202e22b241047
Eleftherios Fanioudakis <eleftherios.fanioudakis@gmail.com>
2024-03-27 15:01:20 +0000

    add client port option

--- components/nw.py
+++ components/nw.py
@@ -5,8 +5,9 @@ class Nw:
     def __init__(self, params=None):
         self.params = params or {}
         self.host_ip = self.params.get('host_ip', None)
-        self.client_target_ip = self.params.get('client_target_ip', None)
         self.port = self.params.get('port', None)
+        self.client_target_ip = self.params.get('client_target_ip', None)
+        self.client_target_port = self.params.get('client_target_port', None)
         
     def server_init(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,6 +23,6 @@ class Nw:
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     def client_connect(self):
-        self.client_socket.connect((self.client_target_ip, self.port))
+        self.client_socket.connect((self.client_target_ip, self.client_target_port))
         ack = self.client_socket.recv(1024)
         return self.client_socket
